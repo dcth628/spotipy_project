43a944dfbdb3b0ae3a9994f9358e03cd781c5458
Dean <dcth628@gmail.com>
2023-12-13 18:07:44 -0800

    try to deploy

--- migrations/versions/96c8b44cf970_.py
+++ migrations/versions/96c8b44cf970_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 7d42ba9bed94
+Revision ID: 96c8b44cf970
 Revises: 
-Create Date: 2023-12-13 17:54:55.947835
+Create Date: 2023-12-13 18:05:33.120971
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '7d42ba9bed94'
+revision = '96c8b44cf970'
 down_revision = None
 branch_labels = None
 depends_on = None
